1aac5c8b5757414037eb551ffde0d8bb990e3b16
Bishnu <canaryedition@gmail.com>
2024-03-24 18:05:35 +0530

    new

--- module_worldometer/wom_downloader.py
+++ module_worldometer/wom_downloader.py
@@ -1,6 +1,6 @@
 import os
 import requests
-
+import re
 # Create a directory to store HTML files if it doesn't exist
 html_dir = 'html'
 if not os.path.exists(html_dir):
@@ -9,9 +9,10 @@ if not os.path.exists(html_dir):
 # Read country names from the text file
 with open('worldometers_countrylist.txt', 'r') as file:
     countries = [line.strip() for line in file]
+    fcountries = [s for s in countries if re.search(r'[A-Za-z\s]', s)]
 
 # Download and save HTML files for each country
-for country in countries:
+for country in fcountries:
     # Construct the URL for the country
     url = f"https://www.worldometers.info/coronavirus/country/{country.lower().replace(' ', '-')}/"
     
