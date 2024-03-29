c09447b7fc6c668f9c242711bdb088a3b447a93f
ChaitanyaKale7 <55970977+ChaitanyaKale7@users.noreply.github.com>
2024-03-29 13:59:08 +0530

    Saving all the countries html

--- module_wikipedia/webpage_download.py
+++ module_wikipedia/webpage_download.py
@@ -3,10 +3,10 @@ import ply.lex as lex
 import ply.yacc as yacc
 from urllib.request import Request, urlopen
 
-url = 'https://en.wikipedia.org/wiki/Responses_to_the_COVID-19_pandemic_in_October_2022'
+url = 'https://en.wikipedia.org/wiki/Timeline_of_the_COVID-19_pandemic_in_Singapore_(2022)'
 req = Request(url ,headers ={'User-Agent':'Mozilla/5.0'})
 webpage = urlopen(req).read()
 mydata = webpage.decode("utf8")
-f=open('Responses/10_2022_Responses.html','w',encoding="utf-8")
+f=open('Countries/Singapore_2022.html','w',encoding="utf-8")
 f.write(mydata)
 f.close
