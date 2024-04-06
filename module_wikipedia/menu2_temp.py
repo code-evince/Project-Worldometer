ce2829e884b83de6c239e72d4750b0ace5659f54
ChaitanyaKale7 <chaitanyaakale007@gmail.com>
2024-04-07 03:24:58 +0530

    improved nosql

--- module_wikipedia/menu2_temp.py
+++ module_wikipedia/menu2_temp.py
@@ -2,10 +2,17 @@ import time
 import sys
 import os
 import subprocess
-import getWikiData
+import locale
+# import getWikiData
 # from getData import main as getCountryData
 import warnings
 import datetime
+import sys
+import nltk
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 warnings.filterwarnings("ignore")
 sys.stderr = open(os.devnull,'w')
 
@@ -48,23 +55,17 @@ def run_map_reduce1(start_date, end_date):
     e_m_year_temp = end_date[3:]
     e_m_year = e_m_year_temp.replace('-', '_')
 
-    print(s_m_year)
-    print(e_m_year)
-
     valid_files = [] #Stores all files between start and end date
 
-    print(f'End year ----------- {e_m_year[3:]}')
     for root, dirs, files in os.walk('CovidNewsTxt/timelines'):
         for file_name in files:
             if len(file_name) == 11:
                 end_year = e_m_year[3:]
                 if end_year > '2019':
                     m_year = file_name[:-4]
-                    # print(m_year)
                     if s_m_year <= m_year <= e_m_year:
                         valid_files.append(file_name)
             else:
-                print(file_name)
                 start_year = s_m_year[3:]
                 end_year = e_m_year[3:]
                 current_year = file_name[0:4]
@@ -72,11 +73,6 @@ def run_map_reduce1(start_date, end_date):
                 if start_year <= current_year <= end_year:
                     valid_files.append(file_name)
             
-
-    print()
-    print(valid_files)
-    print()
-
     cmd = '('
     for file_name in valid_files:
         if len(file_name) == 11:
@@ -94,8 +90,6 @@ def run_map_reduce1(start_date, end_date):
     cmd = cmd + ')'
     
     cmd = cmd + f' |sort -t - -k 3,3n -k 2,2n -k 1,1n |python3 reducer_timelines.py {start_date} {end_date}'
-
-    print(cmd)
 
     subprocess.run(cmd, shell=True)
 
@@ -135,18 +129,147 @@ def run_map_reduce2(start_date, end_date):
 
     subprocess.run(cmd, shell=True)
     
-
+def run_map_reduce3(start_date, end_date, country_name):
+    #Extracting start date and end date in month year formate Eg.01-05-2020 -> 05_2020
+    s_m_year_temp = start_date[3:]
+    s_m_year =  s_m_year_temp.replace('-', '_')
+    start_year = s_m_year[3:]
     
+    
+    e_m_year_temp = end_date[3:]
+    e_m_year = e_m_year_temp.replace('-', '_')
+    end_year = e_m_year[3:]
 
-def run_map_reduce3(file_name, option, country_name):
-    pass
+    valid_files = [] #Stores all files between start and end date
+
+    for root, dirs, files in os.walk('CovidNewsTxt/countries'):
+        for file_name in files:
+            if file_name.startswith(country_name):
+                current_year = file_name[-8:-4]
+                if start_year <= current_year <= end_year:
+                    valid_files.append(file_name)
+
+    valid_files = sorted(valid_files)
+    
+    cmd = '('
+    for file_name in valid_files:
+        year = file_name[-8:-4]
+        cat_cmd = f'cat CovidNewsTxt/countries/{file_name} |' 
+        mapper_cmd = f'python3 mapper_countries.py {year} | sort -nk 1 |'
+        combiner_cmd = f'python3 combiner_countries.py  &'
+
+        cmd += cat_cmd + mapper_cmd + combiner_cmd
+    
+    cmd = cmd[:-1]
+
+    cmd = cmd + ')'
+    
+    cmd = cmd + f' |sort -t - -k 3,3n -k 2,2n -k 1,1n |python3 reducer_countries.py {start_date} {end_date}'
+    
+    subprocess.run(cmd, shell=True)
+    
+def run_map_reduce4(start_date, end_date, country_name):
+    #Extracting start date and end date in month year formate Eg.01-05-2020 -> 05_2020
+    s_m_year_temp = start_date[3:]
+    s_m_year =  s_m_year_temp.replace('-', '_')
+    start_year = s_m_year[3:]
+    
+    
+    e_m_year_temp = end_date[3:]
+    e_m_year = e_m_year_temp.replace('-', '_')
+    end_year = e_m_year[3:]
+
+    valid_files = [] #Stores all files between start and end date
+
+    for root, dirs, files in os.walk('CovidNewsTxt/countries'):
+        for file_name in files:
+            if file_name.startswith(country_name):
+                current_year = file_name[-8:-4]
+                if start_year <= current_year <= end_year:
+                    valid_files.append(file_name)
+
+    valid_files = sorted(valid_files)
+    
+    cmd = '('
+    for file_name in valid_files:
+        year = file_name[-8:-4]
+        cat_cmd = f'cat CovidNewsTxt/countries/{file_name} |' 
+        mapper_cmd = f'python3 mapper_countries.py {year} | sort -nk 1 |'
+        combiner_cmd = f'python3 combiner_countries.py  &'
+
+        cmd += cat_cmd + mapper_cmd + combiner_cmd
+    
+    cmd = cmd[:-1]
+
+    cmd = cmd + ')'
+    
+    cmd = cmd + f' |sort -t - -k 3,3n -k 2,2n -k 1,1n |python3 reducer_countries.py {start_date} {end_date} > {country_name}_result.txt'
+
+    print(cmd)
+    
+    subprocess.run(cmd, shell=True)
+
+def get_jaccard_similarity(file1, file2):
+
+    # Read contents of files
+    with open(file1, 'r') as f1:
+        text1 = f1.read().lower()
+    with open(file2, 'r') as f2:
+        text2 = f2.read().lower()
+
+    # Tokenize text and remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words1 = [word for word in word_tokenize(text1) if word.isalnum() and word not in stop_words]
+    words2 = [word for word in word_tokenize(text2) if word.isalnum() and word not in stop_words]
+
+    # Calculate Jaccard similarity
+    intersection = len(set(words1).intersection(set(words2)))
+    union = len(set(words1).union(set(words2)))
+    similarity = intersection / union if union != 0 else 0
+
+    return similarity
+
+def jaccard_similarity(start_date, end_date, current_country_name):
+    # Read contents of files
+    # List of countries
+    start_year = start_date[-4:]
+    end_year = end_date[-4:]
+
+    countries = ['Australia', 'England', 'India', 'Malaysia', 'Singapore']
+
+    for country in countries:
+        run_map_reduce4(start_date, end_date, country)
+
+    file1 = f'{current_country_name}_result.txt'
+
+    similarity = dict()
+    for country in countries:
+        if country != current_country_name: #Check for all countries except current country
+            if country == 'India' and start_year > 2021:
+                continue
+            else:
+                file2 = f'{country}_result.txt'
+                similarity[country] = get_jaccard_similarity(file1, file2)
+
+    max_value = 0
+    max_key = None
+    for key, value in similarity.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    
+    print(f'{current_country_name} has highest Jaccard Similarity with {max_key}\nJaccard Similarity: {max_value}')
+
 
 def is_valid_date(date_str):
     try:
+        # Set the locale to ensure consistent interpretation of date format
+        locale.setlocale(locale.LC_TIME, 'en_IN.UTF-8')  # Set to 'en_IN.UTF-8' for English, India
+        
         # Attempt to parse the date string
         datetime.strptime(date_str, '%d-%m-%Y')
         return True
-    except ValueError:
+    except Exception as e:
         # If ValueError is raised, it means the date string is not valid
         return False
     
@@ -158,46 +281,148 @@ def main():
         exit = 0
         while(exit==0):
             print("\n-------------- MENU --------------")
-            print("1. COVID-19 World News")
-            print("2. COVID-19 World Responses")
-            print("3. COVID-19 Country-wise News")
-            print("4. Menu")
-            print("5. EXIT")
+            print("1. Wikipedia WCOVID-19 World News")
+            print("2. Wikipedia COVID-19 World Responses")
+            print("3. Wikipedia COVID-19 Country-wise News") #Check if country name exists
+            print("4. Jaccard Similarity for Country")
+            print("5. Menu")
+            print("6. EXIT")
             user = int(input("\nEnter your choice: "))
-            if(user == 5):
+            if(user == 6):
                 exit = 1
                 continue
-            elif(user == 4):
+
+            elif(user == 5):
                 continue
+
             elif(user == 1):
-                if not check_last_updated("checkTimelinesCache.txt"):
-                    print('\nWait!! Downloading & Parsing Countries webpages...\n')
-                    getWikiData.run(1)
+                # if not check_last_updated("checkTimelinesCache.txt"):
+                #     print('\nWait!! Downloading & Parsing Countries webpages...\n')
+                #     getWikiData.run(1)
+
                 start_date = input("Enter the start date[dd-mm-yyyy format]: ")
                 end_date = input("Enter the end date[dd-mm-yyyy format]: ")
+
                 if not is_valid_date(start_date):
                     print('Invalid Start Date')
-                if not is_valid_date(end_date):
+                elif not is_valid_date(end_date):
                     print('Invalid End Date')
+                else:
+                    print('Inside user 1, starting condition passed')
+                    start_year = start_date[-4:]
+                    end_year = end_date[-4:]
+                    
+                    if start_year > '2024':
+                        print('Start Date beyond scope')
+                    elif end_year < '2019':
+                        print('End Date beyond scope')
+                    elif start_year > end_year:
+                        print('Start Date greater than End Date')
+                    else:
+                        print('Reached to function')
+                        run_map_reduce1(start_date, end_date)
+                
+
                 # run_map_reduce1("cache/world.txt",ip,country)
+
             elif(user == 2):
-                if not check_last_updated("checkResponsesCache.txt"):
-                    print('\nWait!! Downloading & Parsing Countries webpages...\n')
-                    getWikiData.run(2)
+                # if not check_last_updated("checkResponsesCache.txt"):
+                #     print('\nWait!! Downloading & Parsing Countries webpages...\n')
+                #     getWikiData.run(2)
+
                 start_date = input("Enter the start date[dd-mm-yyyy format]: ")
                 end_date = input("Enter the end date[dd-mm-yyyy format]: ")
+
+                if not is_valid_date(start_date):
+                    print('Invalid Start Date')
+                elif not is_valid_date(end_date):
+                    print('Invalid End Date')
+                
+                else:
+                    start_year = start_date[-4:]
+                    end_year = end_date[-4:]
+
+                    if start_year > '2022':
+                        print('Start Date beyond scope')
+                    elif end_year < '2020':
+                        print('End Date beyond scope')
+                    elif start_year > end_year:
+                        print('Start Date greater than End Date')
+                    else:
+                        run_map_reduce2(start_date, end_date)
+
                 # run_map_reduce2(start_date, end_date, ip, country)
-            elif(user == 2):
-                if not check_last_updated("checkCountriesCache.txt"):
-                    print('\nWait!! Downloading & Parsing Countries webpages...\n')
-                    getWikiData.run(3)
+
+            elif(user == 3):
+                # if not check_last_updated("checkCountriesCache.txt"):
+                #     print('\nWait!! Downloading & Parsing Countries webpages...\n')
+                #     getWikiData.run(3)
                 country = input("Enter name of the Country: ")
-                start_date = input("Enter the start date[dd-mm-yyyy format]: ")
-                end_date = input("Enter the end date[dd-mm-yyyy format]: ")
-                # country = "Bolivia"
-                # start_date = "08-04-2020"
-                # end_date = "12-04-2022"
-                # run_map_reduce3(start_date, end_date, ip, country)
+
+                countries = ['Australia', 'England', 'India', 'Malaysia', 'Singapore']
+
+                if country not in countries:
+                    print('Country Name not valid')
+                
+                else:
+                    start_date = input("Enter the start date[dd-mm-yyyy format]: ")
+                    end_date = input("Enter the end date[dd-mm-yyyy format]: ")
+
+                    if not is_valid_date(start_date):
+                        print('Invalid Start Date')
+                    elif not is_valid_date(end_date):
+                        print('Invalid End Date')
+                    
+                    else:
+                        start_year = start_date[-4:]
+                        end_year = end_date[-4:]
+
+                        # if start_year > '2022':
+                        #     print('Start Date beyond scope')
+                        if end_year < '2020':
+                            print('End Date beyond scope')
+                        elif start_year > end_year:
+                            print('Start Date greater than End Date')
+                        else:
+                            run_map_reduce3(start_date, end_date, country)
+
+            elif(user == 4):
+                # if not check_last_updated("checkCountriesCache.txt"):
+                #     print('\nWait!! Downloading & Parsing Countries webpages...\n')
+                #     getWikiData.run(3)
+                country = input("Enter name of the Country: ")
+
+                countries = ['Australia', 'England', 'India', 'Malaysia', 'Singapore']
+
+                if country not in countries:
+                    print('Country Name not valid')
+                
+                else:
+                    start_date = input("Enter the start date[dd-mm-yyyy format]: ")
+                    end_date = input("Enter the end date[dd-mm-yyyy format]: ")
+
+                    if not is_valid_date(start_date):
+                        print('Invalid Start Date')
+                    elif not is_valid_date(end_date):
+                        print('Invalid End Date')
+                    
+                    else:
+                        start_year = start_date[-4:]
+                        end_year = end_date[-4:]
+
+                        # if start_year > '2022':
+                        #     print('Start Date beyond scope')
+                        if end_year < '2020':
+                            print('End Date beyond scope')
+                        elif start_year > end_year:
+                            print('Start Date greater than End Date')
+                        elif start_date > 2023:
+                            print('Jaccard Score 0')
+                        else:
+                            jaccard_similarity(start_date, end_date, country) 
+                
+
+
             else:
                 print('Try Again! Enter the valid choice.')
     except:
--- module_wikipedia/temp.py
+++ module_wikipedia/temp.py
@@ -1,5 +1,11 @@
 import os
 import subprocess
+import sys
+import nltk
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 def run_map_reduce2(start_date, end_date):
     #Extracting start date and end date in month year formate Eg.01-05-2020 -> 05_2020
     s_m_year_temp = start_date[3:]
@@ -106,18 +112,154 @@ def run_map_reduce1(start_date, end_date):
 
     subprocess.run(cmd, shell=True)
 
-def get_month(month_name):
-    month_dict = {
-        'January': '01', 'February': '02', 'March': '03', 'April': '04',
-        'May': '05', 'June': '06', 'July': '07', 'August': '08',
-        'September': '09', 'October': '10', 'November': '11', 'December': '12'
-    }
-
+def run_map_reduce3(start_date, end_date, country_name):
+    #Extracting start date and end date in month year formate Eg.01-05-2020 -> 05_2020
+    s_m_year_temp = start_date[3:]
+    s_m_year =  s_m_year_temp.replace('-', '_')
+    start_year = s_m_year[3:]
     
-    month = month_dict[month_name]
+    
+    e_m_year_temp = end_date[3:]
+    e_m_year = e_m_year_temp.replace('-', '_')
+    end_year = e_m_year[3:]
 
-    return month
+    print(f'Start year {start_year}')
+    print(f'End year {end_year}')
 
-run_map_reduce1('15-06-2019', '15-12-2021')
+    valid_files = [] #Stores all files between start and end date
+
+    for root, dirs, files in os.walk('CovidNewsTxt/countries'):
+        for file_name in files:
+            if file_name.startswith(country_name):
+                current_year = file_name[-8:-4]
+                if start_year <= current_year <= end_year:
+                    valid_files.append(file_name)
+
+    valid_files = sorted(valid_files)
+    
+    print()
+    print(valid_files)
+    print()
+
+
+    cmd = '('
+    for file_name in valid_files:
+        year = file_name[-8:-4]
+        cat_cmd = f'cat CovidNewsTxt/countries/{file_name} |' 
+        mapper_cmd = f'python3 mapper_countries.py {year} | sort -nk 1 |'
+        combiner_cmd = f'python3 combiner_countries.py  &'
+
+        cmd += cat_cmd + mapper_cmd + combiner_cmd
+    
+    cmd = cmd[:-1]
+
+    cmd = cmd + ')'
+    
+    cmd = cmd + f' |sort -t - -k 3,3n -k 2,2n -k 1,1n |python3 reducer_countries.py {start_date} {end_date}'
+
+    print(cmd)
+    
+    subprocess.run(cmd, shell=True)
+
+def run_map_reduce4(start_date, end_date, country_name):
+    #Extracting start date and end date in month year formate Eg.01-05-2020 -> 05_2020
+    s_m_year_temp = start_date[3:]
+    s_m_year =  s_m_year_temp.replace('-', '_')
+    start_year = s_m_year[3:]
+    
+    
+    e_m_year_temp = end_date[3:]
+    e_m_year = e_m_year_temp.replace('-', '_')
+    end_year = e_m_year[3:]
+
+    print(f'Start year {start_year}')
+    print(f'End year {end_year}')
+
+    valid_files = [] #Stores all files between start and end date
+
+    for root, dirs, files in os.walk('CovidNewsTxt/countries'):
+        for file_name in files:
+            if file_name.startswith(country_name):
+                current_year = file_name[-8:-4]
+                if start_year <= current_year <= end_year:
+                    valid_files.append(file_name)
+
+    valid_files = sorted(valid_files)
+    
+    print()
+    print(valid_files)
+    print()
+
+
+    cmd = '('
+    for file_name in valid_files:
+        year = file_name[-8:-4]
+        cat_cmd = f'cat CovidNewsTxt/countries/{file_name} |' 
+        mapper_cmd = f'python3 mapper_countries.py {year} | sort -nk 1 |'
+        combiner_cmd = f'python3 combiner_countries.py  &'
+
+        cmd += cat_cmd + mapper_cmd + combiner_cmd
+    
+    cmd = cmd[:-1]
+
+    cmd = cmd + ')'
+    
+    cmd = cmd + f' |sort -t - -k 3,3n -k 2,2n -k 1,1n |python3 reducer_countries.py {start_date} {end_date} > {country_name}_result.txt'
+
+    print(cmd)
+    
+    subprocess.run(cmd, shell=True)
+
+def get_jaccard_similarity(file1, file2):
+
+    # Read contents of files
+    with open(file1, 'r') as f1:
+        text1 = f1.read().lower()
+    with open(file2, 'r') as f2:
+        text2 = f2.read().lower()
+
+    # Tokenize text and remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words1 = [word for word in word_tokenize(text1) if word.isalnum() and word not in stop_words]
+    words2 = [word for word in word_tokenize(text2) if word.isalnum() and word not in stop_words]
+
+    # Calculate Jaccard similarity
+    intersection = len(set(words1).intersection(set(words2)))
+    union = len(set(words1).union(set(words2)))
+    similarity = intersection / union if union != 0 else 0
+
+    return similarity
+
+def jaccard_similarity(start_date, end_date, current_country_name):
+    # Read contents of files
+    # List of countries
+
+    countries = ['Australia', 'England', 'India', 'Malaysia', 'Singapore']
+
+    for country in countries:
+        run_map_reduce4(start_date, end_date, country)
+
+    file1 = f'{current_country_name}_result.txt'
+
+    similarity = dict()
+    for country in countries:
+        if country != current_country_name: #Check for all countries except current country
+            file2 = f'{country}_result.txt'
+            similarity[country] = get_jaccard_similarity(file1, file2)
+    
+    print(similarity)
+
+    max_value = 0
+    max_key = None
+    for key, value in similarity.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    
+    print(f'{current_country_name} has highest Jaccard Similarity with {max_key}\nJaccard Similarity: {max_value}')
+
+
+        
+jaccard_similarity('15-06-2020', '15-12-2021', 'Singapore')
 
 
