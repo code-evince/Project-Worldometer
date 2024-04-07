20db74f9647eeb523375ce5bf9cd8be9ecf34eeb
ayush <aayushshakya333@gmail.com>
2024-04-07 23:01:30 +0530

    Done

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import subprocess
 def main():
     requirements = "pip install nltk; pip install datetime; pip install PLY"
     subprocess.run(requirements, shell=True)
-    
+    print("____________________________________________________________________________________________________________________________")
+    print("____________________________________________________________________________________________________________________________")
+    print('\n\n\n')
+
     print("################################################################")
     print("                        PROJECT COVID-19                        ")
     print("----------------------------------------------------------------")
@@ -17,19 +20,22 @@ def main():
         print("1. Worldometer COVID-19")
         print("2. Wikipedia COVID-19")
         print("3. EXIT")
-        user = int(input("\nEnter your choice: "))
-        if(user == 3):
-            exit = 1
-        elif(user == 1):
-            cmd = "python3 module_worldometer/menu.py"
-            subprocess.run(cmd, shell=True)
-            pass
-        elif(user == 2):
-            cmd = "python3 module_wikipedia/menu2.py"
-            subprocess.run(cmd, shell=True)
-            pass
-        else:
-            print('Try Again! Enter the valid choice.')
+        try:
+            user = int(input("\nEnter your choice: "))
+            if(user == 3):
+                exit = 1
+            elif(user == 1):
+                cmd = "python3 module_worldometer/menu.py"
+                subprocess.run(cmd, shell=True)
+                pass
+            elif(user == 2):
+                cmd = "python3 module_wikipedia/menu2.py"
+                subprocess.run(cmd, shell=True)
+                pass
+            else:
+                print('Try Again! Enter the valid choice.')
+        except Exception as er:
+            print(er)
 
 
 if __name__ == '__main__':
